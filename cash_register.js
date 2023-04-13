43e14169c5e319b6066516e12c4ff626ac9b009e
Aoniq <89135@glr.nl>
2023-04-13 13:55:26 +0200

    Initial commit

---
+++ cash_register.js
@@ -0,0 +1,28 @@
+//1. Create variables
+let totalPriceHamburger = 0;
+
+//2. Main function to update the number and price for each snack
+function addSnack(snackName, snackPrice, snackID) {
+    switch (snackName) {
+        case "Hamburger":
+            //Increase number of hamburgers
+            let howManyHamburger = Number(document.getElementById(`howMany${snackID}`).value);
+            howManyHamburger++;
+            document.getElementById(`howMany${snackID}`).value = howManyHamburger;
+            
+            //Increase total price of hamburgers
+            totalPriceHamburger = (howManyHamburger * snackPrice);
+            totalPriceHamburger = roundIt(totalPriceHamburger);
+            document.getElementById(`totalPrice${snackID}`).value = totalPriceHamburger;
+            break;
+ 
+    }
+    //Update total amount for this order
+    document.getElementById("totalOrderAmount").value = Number(totalPriceHamburger).toFixed(2);
+}
+
+//3. Function to round the amounts to 2 decimals
+function roundIt(amountToRound) {
+    amountToRound = Math.round(amountToRound * 100)/100;
+    return amountToRound;
+}
---
+++ cash_register.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MacMedia Cash Register</title>
+    <script src="./cash_register.js" defer></script>
+</head>
+
+<body>
+    <h1>Welcome to MacMedia</h1>
+    <?php
+        //Connect to database
+        $db = new SQLite3("./dbMacMedia.db");
+        $db->busyTimeout(5000);
+        
+        //Create query and execute query
+        $query = "SELECT * FROM snacks";
+        $result = $db->query($query);
+    ?>
+    <table>
+    <?php
+        //Read results from query and create the button, number and price field for each result
+        while($snack = $result->fetchArray(SQLITE3_ASSOC)) {?>
+            <tr>
+                <td><button type="button" onclick="addSnack('<?php echo $snack['snackName']?>', <?php echo $snack['snackPrice']?>, '<?php echo $snack['ID']?>')">
+                    <?php echo $snack['snackName']?> <br> <?php echo $snack['snackPrice'];?></button></td>
+                <td>How many: <input type="text" id="howMany<?php echo $snack['ID']?>" disabled></td>
+                <td>Price: <input type="text" id="totalPrice<?php echo $snack['ID']?>" disabled></td>
+            </tr>
+    <?php }; ?>
+    </table>
+    <p>
+        <strong>Total amount for this order:</strong>
+        <input type="text" id="totalOrderAmount" disabled>
+        <button type="button">Check out</button>
+    </p>
+ </body>
+
+</body>
+</html>
